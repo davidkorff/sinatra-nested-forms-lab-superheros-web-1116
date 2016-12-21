bd1a977d70de251f6e9015b4b8c8af827b607a06
github username <github email address>
2016-12-21 14:13:03 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,27 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @team_name = params["team"]["name"]
+      @team_motto = params["team"]["motto"]
+      @team_member_1_name = params["team"]["members"][0]["name"]
+      @team_member_1_power = params["team"]["members"][0]["power"]
+      @team_member_1_bio = params["team"]["members"][0]["bio"]
+      @team_member_2_name = params["team"]["members"][1]["name"]
+      @team_member_2_power = params["team"]["members"][1]["power"]
+      @team_member_2_bio = params["team"]["members"][1]["bio"]
+      @team_member_3_name = params["team"]["members"][2]["name"]
+      @team_member_3_power = params["team"]["members"][2]["power"]
+      @team_member_3_bio = params["team"]["members"][2]["bio"]
+
+      #binding.pry
+
+      erb :teams
+    end
+
 
 end
